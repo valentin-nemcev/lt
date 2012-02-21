a1b42136cdd1c985559e27ab2a4bba5635c04ab3
Валентин Немцев <mail@valentin-nemcev.com>
2012-02-21 15:41:08 +0400

    Cleanup controller

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -16,18 +16,6 @@ class TasksController < ApplicationController
     @task = Task.find(params[:id])
 
     respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @task }
-    end
-  end
-
-  # GET /tasks/new
-  # GET /tasks/new.json
-  def new
-    @task = Task.new
-
-    respond_to do |format|
-      format.html # new.html.erb
       format.json { render json: @task }
     end
   end
@@ -44,10 +32,8 @@ class TasksController < ApplicationController
 
     respond_to do |format|
       if @task.save
-        format.html { redirect_to @task, notice: 'Task was successfully created.' }
         format.json { render json: @task, status: :created, location: @task }
       else
-        format.html { render action: "new" }
         format.json { render json: @task.errors, status: :unprocessable_entity }
       end
     end
@@ -60,10 +46,8 @@ class TasksController < ApplicationController
 
     respond_to do |format|
       if @task.update_attributes(params[:task])
-        format.html { redirect_to @task, notice: 'Task was successfully updated.' }
-        format.json { head :no_content }
+        format.json { render json: @task, status: :ok}
       else
-        format.html { render action: "edit" }
         format.json { render json: @task.errors, status: :unprocessable_entity }
       end
     end
@@ -76,7 +60,6 @@ class TasksController < ApplicationController
     @task.destroy
 
     respond_to do |format|
-      format.html { redirect_to tasks_url }
       format.json { head :no_content }
     end
   end
