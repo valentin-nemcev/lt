061ce3160504c9faf4ffa0cc54cc96b88e1ddff3
Валентин Немцев <mail@valentin-nemcev.com>
2012-04-10 15:18:04 +0400

    Current user is saved in session

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
 
   def select
+    session[:user_id] = params[:user_id]
     redirect_to root_url
   end
 
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -3,9 +3,17 @@ module UsersHelper
     users = User.all
     content_tag :div, :id => 'user_selector' do
       form_tag select_user_path do
-        opts = options_from_collection_for_select users, :id, :name
-        select_tag 'user', opts
+        user_id = current_user.id
+        opts = options_from_collection_for_select users, :id, :name, user_id
+        select_tag :user_id, opts
       end
     end
+  end
+
+  def current_user
+    users = User.scoped
+    user_id = session[:user_id]
+    users = users.where :id => user_id unless user_id.nil?
+    users.first!
   end
 end
