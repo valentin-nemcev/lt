78929ecd6f0ffa25818b30db4273e52a85a5f4a8
Валентин Немцев <mail@valentin-nemcev.com>
2012-06-24 16:17:47 +0400

    Task mapper stub

--- app/models/task/mapper.rb
+++ app/models/task/mapper.rb
@@ -1,36 +1,62 @@
 module Task
-class Mapper
-  def self.create(attrs = {})
-    Record.new.tap do |task|
-      task.body = attrs.fetch(:body)
-      current_time = Time.current
-      task.created_on = attrs.fetch(:on, current_time)
+  class Mapper
+    def self.create(attrs = {})
+      Record.new.tap do |task|
+        task.body = attrs.fetch(:body)
+        current_time = Time.current
+        task.created_on = attrs.fetch(:on, current_time)
 
-      task.parent_id = attrs[:project].id if attrs.has_key? :project
+        task.parent_id = attrs[:project].id if attrs.has_key? :project
 
-      task.user = attrs[:user]
+        task.user = attrs[:user]
 
-      task.effective_date = [current_time, task.created_on].max
+        task.effective_date = [current_time, task.created_on].max
+        task.save!
+      end
+    end
+
+    def self.fetch_all(conditions = {})
+      t = Record.scoped
+
+      if user = conditions[:for_user]
+        t = t.where(user_id: user.id)
+      end
+
+      t.all
+    end
+
+    def self.fetch_by_id(task_id)
+      Record.find(task_id)
+    end
+
+    def self.save(task)
       task.save!
     end
-  end
 
-  def self.fetch_all(conditions = {})
-    t = Record.scoped
 
-    if user = conditions[:for_user]
-      t = t.where(user_id: user.id)
+
+    def get_new_id
+      @last_id ||= 0
+      @last_id += 1
     end
 
-    t.all
-  end
+    def storage
+      @storage ||= {}
+    end
 
-  def self.fetch_by_id(task_id)
-    Record.find(task_id)
-  end
+    def save(task)
+      id = if task.persisted? then task.id else get_new_id end
+      storage[id] = task
+      task.id = id unless task.persisted?
+    end
 
-  def self.save(task)
-    task.save!
+    def load_all
+      storage.values
+    end
+
+    def load_by_id id
+      storage[id]
+    end
+
   end
-end
 end
--- spec/models/task/mapper_spec.rb
+++ spec/models/task/mapper_spec.rb
@@ -1,117 +1,45 @@
 require 'spec_helper'
 
-TaskMapper = Task::Mapper
-describe TaskMapper do
-  describe '.create' do
-    it 'creates tasks with given body' do
-      task = TaskMapper.create body: 'hello!'
-      task.body.should eq('hello!')
-    end
+describe 'Task persistance' do
 
-    it 'creates tasks with default creation date' do
-      with_frozen_time do |now|
-        task = TaskMapper.create body: 'hello!'
-        task.created_on.should eq(now)
-      end
-    end
+  let(:mapper) { Task::Mapper.new }
+  let(:task)  { double("Task",   persisted?: false) }
+  let(:task1) { double("Task 1", persisted?: false) }
+  let(:task2) { double("Task 2", persisted?: false) }
+  let(:persisted_task) {
+    double("Persisted task", persisted?: true, id: 1)
+  }
 
-    it 'creates tasks with provided creation date' do
-      with_frozen_time do |now|
-        task = TaskMapper.create body: 'hello!', on: 2.days.since(now)
-        task.created_on.should eq(2.days.since(now))
-      end
-    end
-
-    it 'creates tasks with provided project' do
-      project = TaskMapper.create body: 'project!'
-      task = TaskMapper.create body: 'task!', project: project
-      task.project.should eq(project)
-      project.subtasks.should eq([task])
-    end
-
-    it 'creates tasks with provided user' do
-      user = User.create!
-      task = TaskMapper.create body: 'hello!', user: user
-      task.user.should eq(user)
-    end
-
-    describe 'creates tasks with valid effective date' do
-      specify 'on current date' do
-        with_frozen_time do |now|
-          task = create_task
-          task.effective_date.should eq(now)
-        end
-      end
-
-      specify 'on past date' do
-        with_frozen_time do |now|
-          task = create_task on: 2.days.ago
-          task.effective_date.should eq(now)
-        end
-      end
-
-      specify 'on future date' do
-        with_frozen_time do |now|
-          task = create_task on: 2.days.from_now
-          task.effective_date.should eq(2.days.from_now)
-        end
-      end
-    end
+  it 'should load and save tasks' do
+    mapper.save task.as_null_object
+    mapper.load_all.should == [task]
   end
 
-  def create_task(attrs={})
-    attrs.merge! body: 'Test task'
-    TaskMapper.create attrs
+  it 'should load tasks with specified ids' do
+    id1 = nil
+    task.should_receive('id=').with(anything) { |id| id1 = id }
+    mapper.save task
+    loaded_task = mapper.load_by_id id1
+    loaded_task.should == task
   end
 
-  describe '.fetch_all' do
-    it 'fetches tasks' do
-      task1 = create_task
-      task2 = create_task
-      fetched_tasks = TaskMapper.fetch_all
-      fetched_tasks.should =~ [task1, task2]
+
+  describe '#save' do
+    it 'should assing ids to newly persisted tasks' do
+      id1 = id2 = nil
+      task1.should_receive('id=').with(anything) { |id| id1 = id }
+      task2.should_receive('id=').with(anything) { |id| id2 = id }
+      mapper.save task1
+      mapper.save task2
+      id1.should_not == id2
     end
 
-    it 'fetches tasks for provided user' do
-      user1 = User.create! login: 'user1'
-      user2 = User.create! login: 'user2'
-      task1 = create_task user: user1
-      task2 = create_task user: user2
-
-      fetched_tasks = TaskMapper.fetch_all for_user: user1
-
-      fetched_tasks.should =~ [task1]
+    it 'should update already persisted tasks' do
+      mapper.save persisted_task
+      mapper.save persisted_task
+      mapper.load_all.should == [persisted_task]
     end
 
-    it 'fetches task as of current date by default' do
-      create_task
-
-    end
   end
 
-  describe '.fetch_by_id' do
-    it 'fetches single task by id' do
-      task = create_task
-      fetched_task = TaskMapper.fetch_by_id task.id
-
-      fetched_task.should eq(task)
-    end
-  end
-
-  describe '.save' do
-    it 'saves task completion state' do
-      task = create_task
-      task.complete!
-      TaskMapper.save task
-
-      fetched_task = TaskMapper.fetch_by_id task.id
-      fetched_task.should eq(task)
-
-      task.undo_complete!
-      TaskMapper.save task
-
-      fetched_task = TaskMapper.fetch_by_id task.id
-      fetched_task.should eq(task)
-    end
-  end
 end
