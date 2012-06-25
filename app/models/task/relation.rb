844aa9eecda87452153c9a5bffe6d4f19ef3206d
Валентин Немцев <mail@valentin-nemcev.com>
2012-06-25 15:05:43 +0400

    Checks for relation addition and removal dates

--- app/models/task/relation.rb
+++ app/models/task/relation.rb
@@ -1,25 +1,35 @@
-class Task::Relation
+module Task
+  class InvalidRelationError < StandardError; end;
+  class Relation
 
-  include Graph::Edge
+    include Graph::Edge
 
-  attr_reader :type, :added_on, :removed_on
-  def initialize(attrs={})
-    self.nodes.parent = attrs.fetch :supertask
-    self.nodes.child = attrs.fetch :subtask
-    @type = attrs.fetch :type
-    @added_on = attrs.fetch :on, Time.current
+    attr_reader :type, :added_on, :removed_on
+    def initialize(attrs={})
+      self.nodes.parent = attrs.fetch :supertask
+      self.nodes.child = attrs.fetch :subtask
+      @type = attrs.fetch :type
+      @added_on = attrs[:on] || attrs[:added_on] || Time.current
+      remove on: attrs[:removed_on]
+    end
+
+    def remove(opts={})
+      removed_on = opts.fetch :on, Time.current
+      if removed_on && removed_on < added_on
+        raise InvalidRelationError,
+          "Relation couldn't be removed earlier than it was created"
+      end
+      @removed_on = removed_on
+      return self
+    end
+
+    def dependency?
+      type == :dependency
+    end
+
+    def composition?
+      type == :composition
+    end
+
   end
-
-  def remove(opts={})
-    @removed_on = opts.fetch :on, Time.current
-  end
-
-  def dependency?
-    type == :dependency
-  end
-
-  def composition?
-    type == :composition
-  end
-
 end
--- spec/models/task/relation_spec.rb
+++ spec/models/task/relation_spec.rb
@@ -1,6 +1,64 @@
 require 'spec_helper'
 
 describe Task::Relation do
-  # TODO: Spec date consitency
+  class TaskStub
+    include Graph::Node
+  end
+
+  def create_task
+    TaskStub.new
+  end
+
+  def create_relation(attrs={})
+    defaults = {supertask: create_task, subtask: create_task, type: test_type}
+    described_class.new defaults.merge(attrs)
+  end
+
+
+  let(:test_type)  { :dependency }
+  let(:test_date0) { 7.hours.ago }
+  let(:test_date1) { 4.hours.ago }
+  let(:test_date2) { 2.hours.ago }
+
+  context 'created without addition date' do
+    let(:relation) { create_relation }
+
+    it 'should have default addition date', :with_frozen_time do
+      relation.added_on.should eq(Time.current)
+    end
+  end
+
+  context 'created with addition date' do
+    let(:relation_on)       { create_relation       on: test_date1 }
+    let(:relation_added_on) { create_relation added_on: test_date1 }
+
+    it 'should have passed addition date' do
+      relation_on.added_on.should eq(test_date1)
+      relation_added_on.added_on.should eq(test_date1)
+    end
+
+    it "couldn't be removed earlier than it was created" do
+      expect do
+        relation_on.remove on: test_date0
+      end.to raise_error Task::InvalidRelationError
+    end
+  end
+
+  context 'created with addition and valid removal date' do
+    let(:relation) do
+      create_relation added_on: test_date1, removed_on: test_date2
+    end
+
+    it 'should have addition and removal dates' do
+      relation.added_on.should   eq(test_date1)
+      relation.removed_on.should eq(test_date2)
+    end
+
+    it 'should allow "unremoving" by setting removal date to nil' do
+      relation.remove on: nil
+      relation.removed_on.should be_nil
+    end
+  end
+
   # TODO: Spec loop check
 end
