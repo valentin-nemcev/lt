4045df9334f5b83c0b6405014bddfeca8175bf0c
Валентин Немцев <mail@valentin-nemcev.com>
2012-06-30 15:03:51 +0400

    New mapper for old db schema

---
+++ app/models/task/legacy_mapper.rb
@@ -0,0 +1,47 @@
+module Task
+  class LegacyMapper
+
+    def initialize
+      @task_scope = Record.scoped
+    end
+
+    def records
+      @task_scope.all
+    end
+
+    def for_user(user)
+      @task_scope = @task_scope.where(user_id: user.id)
+      self
+    end
+
+    def fetch_all
+      tasks = {}
+      records.each do |record|
+        task = map_record_to_task record
+        tasks[record.id] = task
+        unless record.root?
+          parent_task = tasks[record.parent.id]
+          Relation.new(
+            subtask: task,
+            supertask: parent_task,
+            type: :composition,
+            added_on: [task, parent_task].map(&:created_on).max
+          )
+        end
+      end
+      return tasks.values
+    end
+
+    def map_record_to_task record
+      fields = {
+        id: record.id,
+        created_on: record.created_on,
+        completed_on: record.completed_on,
+        objective: record.body,
+      }
+      cls = record.leaf? ? Action : Project
+      cls.new fields
+    end
+
+  end
+end
--- app/models/task/record.rb
+++ app/models/task/record.rb
@@ -1,109 +1,12 @@
 module Task
-class Record < ActiveRecord::Base
-  self.table_name = 'tasks'
-  class TaskDateInvalid < StandardError; end;
+  class Record < ActiveRecord::Base
+    self.table_name = 'tasks'
 
-  has_dag_links :link_class_name => 'Task::Dependency', :prefix => 'dependency'
-  alias_method :blocking_tasks, :dependency_ancestors
+    acts_as_nested_set
 
-  acts_as_nested_set
-  alias_method :subtasks, :children
-  alias_method :project, :parent
+    default_scope order('lft')
 
-  attr_accessible :position, :id, :parent_id, :body
+    belongs_to :user
 
-  default_scope order('lft')
-
-  belongs_to :user
-
-  attr_writer :position
-
-  def position
-    @position ||= current_position
   end
-
-  after_save :move_to_position
-
-  def move_to_position
-    return unless @position
-    move_to @position[:of], @position[:to].to_sym
-  end
-
-  def current_position
-    if self.root?
-      { to: 'root' }
-    else
-      { to: 'child', of: self.parent_id }
-    end
-  end
-
-
-  def self.as_of(date)
-    effective_date = self.sanitize_sql ['? AS `effective_date`', date]
-    select(['tasks.*', effective_date]).where('? >= created_on', date)
-  end
-
-
-  def complete!(opts={})
-    raise "Project could not be completed directly" if project?
-    on = opts.fetch(:on, effective_date)
-    self.completed_on = on
-    return self
-  end
-
-  def undo_complete!
-    write_attribute(:completed_on, nil)
-    return self
-  end
-
-
-  def as_of(date)
-    clone.tap { |t| t.effective_date = date }
-  rescue TaskDateInvalid
-    nil
-  end
-
-  def effective_date=(date)
-    if date < self.created_on
-      raise TaskDateInvalid, "Task didn't exist as of #{date}"
-    end
-    @effective_date = date
-    return self
-  end
-
-  def effective_date
-    @effective_date ||= if cd = attributes['effective_date']
-      Time.zone.parse cd
-    else
-      Time.zone.now
-    end
-  end
-
-  def completed
-    if project?
-      return leaves.all?(&:completed?) && !blocked?
-    end
-
-    if completed_on
-      completed_on <= effective_date
-    else
-      false
-    end
-  end
-  alias_method :completed?, :completed
-
-  def actionable
-    not project? and not completed? and not blocked?
-  end
-  alias_method :actionable?, :actionable
-
-  def project?
-    not leaf?
-  end
-
-  def blocked?
-    not blocking_tasks.all?(&:completed?)
-  end
-
-end
 end
