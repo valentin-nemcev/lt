1a96a9910e8b1b63fbece42c6c32f4ae9a917f6a
Валентин Немцев <mail@valentin-nemcev.com>
2012-07-17 22:13:59 +0400

    Fix namespace problem in models

--- app/models/task/records/task.rb
+++ app/models/task/records/task.rb
@@ -3,7 +3,8 @@ module Task
     class Task < ActiveRecord::Base
       belongs_to :user
       has_many :objective_revisions,
-        :class_name => ObjectiveRevision, :dependent => :destroy
+        :class_name => ::Task::Records::ObjectiveRevision,
+        :dependent => :destroy
 
       attr_accessible :created_on
     end
