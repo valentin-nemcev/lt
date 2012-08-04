06b7dd2e04763243acd9219ce27a2fb792f89112
Валентин Немцев <mail@valentin-nemcev.com>
2012-08-05 00:09:44 +0400

    Acceptance test for task types

--- spec/acceptance/task_creation_spec.rb
+++ spec/acceptance/task_creation_spec.rb
@@ -1,22 +1,21 @@
 require 'spec_helper'
 
 feature "Task creation", :acceptance do
-  before(:all) { create_test_user }
+  before(:each) { create_test_user; visit tasks_page }
+
+  let(:tasks) { find('[widget=tasks]') }
 
   scenario 'Creating an action' do
-    visit tasks_page
-
-    tasks = find('[widget=tasks]')
-
     tasks.find('[control=new]').click
     task = tasks.find('[record=task][record-state=new]')
     task.find('[form=new-task]').tap do |form|
-      form.find('[input=type]').set('action')
+      form.find('[input=type][value=action]').set(true)
       form.find('[input=objective]').set('Test objective')
       form.find('[control=save]').click
     end
 
     task.should match_selector('[record-state!=new]')
+    task.should match_selector('[task-type=action]')
     task_id = task['record-id']
     task_id.should_not be_nil
 
@@ -28,5 +27,21 @@ feature "Task creation", :acceptance do
     tasks.should have_selector("[record=task][record-id='#{task_id}']")
   end
 
+  scenario 'Creating a project' do
+    tasks.find('[control=new]').click
+    task = tasks.find('[record=task][record-state=new]')
+    task.find('[form=new-task]').tap do |form|
+      form.find('[input=type][value=project]').set(true)
+      form.find('[input=objective]').set('Test project objective')
+      form.find('[control=save]').click
+    end
+
+    task.should match_selector('[task-type=project]')
+    task_id = task['record-id']
+
+    reload_page
+    task = find("[record=task][record-id='#{task_id}']")
+    task.should match_selector('[task-type=project]')
+  end
   scenario 'Creating task without objective', :pending
 end
