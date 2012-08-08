e0dae4e483f9207c7c1915a9a726359dbfb02111
Валентин Немцев <mail@valentin-nemcev.com>
2012-08-09 00:29:33 +0400

    Better error handling for task subtype creation

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,9 +1,11 @@
 # TODO: rename to Tasks
 module Task
-
+  class TaskSubtypeError < StandardError; end;
   def self.new_subtype(subtype, *args)
-    # TODO: Add check for empty or nonexistent subtype
+    fail TaskSubtypeError, 'Empty task subtype' if subtype.blank?
     subtype = const_get(subtype.to_s.camelize)
     subtype.new *args
+  rescue NameError
+    fail TaskSubtypeError, "Unknown task subtype: #{subtype}"
   end
 end
