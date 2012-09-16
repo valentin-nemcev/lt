1967a598058ede40df2217a13ad208b43b15a151
Валентин Немцев <mail@valentin-nemcev.com>
2012-09-16 14:22:59 +0400

    after 60e9f

--- spec/lib/revisions/revision_spec.rb
+++ spec/lib/revisions/revision_spec.rb
@@ -6,14 +6,31 @@ include Revisions
 describe Revision do
   subject(:revision) { Revision.new initialize_args}
 
-  let(:initialize_args) { {:updated_value => :attr_value} }
+  let(:initialize_args) { {
+    :updated_value => :attr_value,
+    :updated_on => :updated_on,
+  } }
 
-  describe '#intialize' do
+  context 'with owner passed on creation' do
+    before(:each) { initialize_args[:owner] = owner }
+    its(:owner) { should eq(owner) }
+  end
+
+  context 'with owner' do
+    before(:each) { revision.owner = owner }
+    its(:owner) { should eq(owner) }
+
+    specify do
+      expect{ revision.owner = stub(:new_owner) }.to raise_error(RevisionError)
+    end
   end
 
   describe '#updated_value' do
     specify do
       revision.updated_value.should eq(:attr_value)
+      revision.updated_on.should eq(:updated_on)
     end
   end
+
+  let(:owner) { stub(:owner) }
 end
