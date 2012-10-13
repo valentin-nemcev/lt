686264c776ead72711a658b5ac36f5d87677fc6e
Валентин Немцев <mail@valentin-nemcev.com>
2012-10-14 01:58:13 +0400

    Create Interval helper class

---
+++ lib/interval.rb
@@ -0,0 +1,103 @@
+class Interval
+  def initialize(endpoints = {})
+    @left_endpoint  = endpoints[:left_open]  || endpoints[:left_closed]
+    @right_endpoint = endpoints[:right_open] || endpoints[:right_closed]
+
+    @left_open  = left_unbounded?  || endpoints.has_key?(:left_open)
+    @right_open = right_unbounded? || endpoints.has_key?(:right_open)
+  end
+  attr_reader :left_endpoint, :right_endpoint
+
+  def inspect
+    lb = left_open?  ? '(' : '['
+    rb = right_open? ? ')' : ']'
+    "#<Interval #{lb}#{left_endpoint.inspect}, #{right_endpoint.inspect}#{rb}>"
+  end
+
+  def overlaps_with?(other)
+    self.include_left?(
+      other.left_endpoint, other.left_open?, other.left_unbounded?) &&
+      self.include_right?(
+        other.right_endpoint, other.right_open?, other.right_unbounded?)
+  end
+
+  def include?(el)
+    include_left?(el) && include_right?(el)
+  end
+
+  def include_left?(el, el_open = false, el_unbounded = false)
+    right_unbounded? or el_unbounded or if right_open? || el_open
+      right_endpoint > el
+    else
+      right_endpoint >= el
+    end
+  end
+
+  def include_right?(el, el_open = false, el_unbounded = false)
+    left_unbounded? or el_unbounded or if left_open? || el_open
+      left_endpoint < el
+    else
+      left_endpoint <= el
+    end
+  end
+
+  def proper?
+    !empty? && !degenerate?
+  end
+
+  def empty?
+    !overlaps_with? self
+  end
+
+  def degenerate?
+    closed? && left_endpoint == right_endpoint
+  end
+
+  def open?
+    left_open? && right_open?
+  end
+
+  def closed?
+    left_closed? && right_closed?
+  end
+
+  def bounded?
+    left_bounded? && right_bounded?
+  end
+
+  def unbounded?
+    left_unbounded? && right_unbounded?
+  end
+
+  def left_unbounded?
+    @left_endpoint.nil?
+  end
+
+  def right_unbounded?
+    @right_endpoint.nil?
+  end
+
+  def left_bounded?
+    !left_unbounded?
+  end
+
+  def right_bounded?
+    !right_unbounded?
+  end
+
+  def left_open?
+    @left_open
+  end
+
+  def right_open?
+    @right_open
+  end
+
+  def left_closed?
+    not @left_open
+  end
+
+  def right_closed?
+    not @right_open
+  end
+end
---
+++ spec/lib/interval_spec.rb
@@ -0,0 +1,522 @@
+require 'lib/spec_helper'
+require 'interval'
+
+describe Interval do
+  describe '#include?' do
+    example("
+    123456789
+      [---]
+     x
+    ") { Interval.new(left_closed: 3, right_closed: 7).should_not include(2) }
+
+    example("
+    123456789
+      (---]
+      x
+    ") { Interval.new(left_open: 3, right_closed: 7).should_not include(3) }
+
+    example("
+    123456789
+      [---]
+       x
+    ") { Interval.new(left_closed: 3, right_closed: 7).should include(4) }
+
+    example("
+    123456789
+      [---)
+          x
+    ") { Interval.new(left_closed: 3, right_open: 7).should_not include(7) }
+
+    example("
+    123456789
+      [------
+          x
+    ") { Interval.new(left_closed: 3, right_open: nil).should include(7) }
+
+    example("
+    123456789
+    ---------
+          x
+    ") { Interval.new(left_open: nil, right_open: nil).should include(7) }
+
+    example("
+    123456789
+    -------]
+          x
+    ") { Interval.new(left_open: nil, right_closed: 8).should include(7) }
+
+    example("
+    123456789
+    ------)
+          x
+    ") { Interval.new(left_open: nil, right_open: 7).should_not include(7) }
+
+    example("
+    123456789
+      (------
+      x
+    ") { Interval.new(left_open: 3, right_open: nil).should_not include(3) }
+  end
+
+  describe '#overlaps_with?' do
+    RSpec::Matchers.define :overlap_with do |other_interval|
+      match do |interval|
+        interval.overlaps_with? other_interval
+      end
+    end
+
+    example "
+    123456789
+    [--]
+        [---]
+    " do
+      iv1 = Interval.new left_closed: 1, right_closed: 4
+      iv2 = Interval.new left_closed: 5, right_closed: 9
+      iv1.should_not overlap_with iv2
+      iv2.should_not overlap_with iv1
+    end
+
+    example "
+    123456789
+    [---]
+        [---]
+    " do
+      iv1 = Interval.new left_closed: 1, right_closed: 5
+      iv2 = Interval.new left_closed: 5, right_closed: 9
+      iv1.should overlap_with iv2
+      iv2.should overlap_with iv1
+    end
+
+    example "
+    123456789
+    [---)
+        [---]
+    " do
+      iv1 = Interval.new left_closed: 1, right_open:   5
+      iv2 = Interval.new left_closed: 5, right_closed: 9
+      iv1.should_not overlap_with iv2
+      iv2.should_not overlap_with iv1
+    end
+
+    example "
+    123456789
+    [----]
+        [---]
+    " do
+      iv1 = Interval.new left_closed: 1, right_closed: 5
+      iv2 = Interval.new left_closed: 5, right_closed: 9
+      iv1.should overlap_with iv2
+      iv2.should overlap_with iv1
+    end
+
+    example "
+    123456789
+    [------]
+      [---]
+    " do
+      iv1 = Interval.new left_closed: 1, right_closed: 5
+      iv2 = Interval.new left_closed: 5, right_closed: 9
+      iv1.should overlap_with iv2
+      iv2.should overlap_with iv1
+    end
+
+    example "
+    123456789
+    (------)
+    [------]
+    " do
+      iv1 = Interval.new left_closed: 1, right_closed: 5
+      iv2 = Interval.new left_closed: 5, right_closed: 9
+      iv1.should overlap_with iv2
+      iv2.should overlap_with iv1
+    end
+
+    example "
+    123456789
+    (------)
+    (------)
+    " do
+      iv1 = Interval.new left_closed: 1, right_closed: 5
+      iv2 = Interval.new left_closed: 5, right_closed: 9
+      iv1.should overlap_with iv2
+      iv2.should overlap_with iv1
+    end
+
+
+    example "
+    123456789
+    [------]
+    [------]
+    " do
+      iv1 = Interval.new left_closed: 1, right_closed: 5
+      iv2 = Interval.new left_closed: 5, right_closed: 9
+      iv1.should overlap_with iv2
+      iv2.should overlap_with iv1
+    end
+
+    example "
+    123456789
+       [-----
+    -------]
+    " do
+      iv1 = Interval.new left_closed: 4, right_open: nil
+      iv2 = Interval.new left_open: nil, right_closed: 8
+      iv1.should overlap_with iv2
+      iv2.should overlap_with iv1
+    end
+
+    example "
+    123456789
+       (-----
+    -------)
+    " do
+      iv1 = Interval.new left_open: 4, right_open: nil
+      iv2 = Interval.new left_open: nil, right_open: 8
+      iv1.should overlap_with iv2
+      iv2.should overlap_with iv1
+    end
+
+    example "
+    123456789
+       [-----
+    -------]
+    " do
+      iv1 = Interval.new left_closed: 4, right_open: nil
+      iv2 = Interval.new left_open: nil, right_closed: 8
+      iv1.should overlap_with iv2
+      iv2.should overlap_with iv1
+    end
+
+    example "
+    123456789
+       [-----
+    -------]
+    " do
+      iv1 = Interval.new left_closed: 4, right_open: nil
+      iv2 = Interval.new left_open: nil, right_closed: 8
+      iv1.should overlap_with iv2
+      iv2.should overlap_with iv1
+    end
+
+    example "
+    123456789
+       [-----
+    -------]
+    " do
+      iv1 = Interval.new left_closed: 4, right_open: nil
+      iv2 = Interval.new left_open: nil, right_closed: 8
+      iv1.should overlap_with iv2
+      iv2.should overlap_with iv1
+    end
+
+    example "
+    123456789
+       [-----
+    -------]
+    " do
+      iv1 = Interval.new left_closed: 4, right_open: nil
+      iv2 = Interval.new left_open: nil, right_closed: 8
+      iv1.should overlap_with iv2
+      iv2.should overlap_with iv1
+    end
+
+    example "
+    123456789
+       [-----
+    -------]
+    " do
+      iv1 = Interval.new left_closed: 4, right_open: nil
+      iv2 = Interval.new left_open: nil, right_closed: 8
+      iv1.should overlap_with iv2
+      iv2.should overlap_with iv1
+    end
+
+    example "
+    123456789
+       [-----
+    -------]
+    " do
+      iv1 = Interval.new left_closed: 4, right_open: nil
+      iv2 = Interval.new left_open: nil, right_closed: 8
+      iv1.should overlap_with iv2
+      iv2.should overlap_with iv1
+    end
+
+    example "
+    123456789
+       (-----
+    ---)
+    " do
+      iv1 = Interval.new left_open: 4, right_open: nil
+      iv2 = Interval.new left_open: nil, right_open: 4
+      iv1.should_not overlap_with iv2
+      iv2.should_not overlap_with iv1
+    end
+
+    example "
+    123456789
+     [-------
+       [-----
+    " do
+      iv1 = Interval.new left_closed: 2, right_open: nil
+      iv2 = Interval.new left_closed: 4, right_open: nil
+      iv1.should overlap_with iv2
+      iv2.should overlap_with iv1
+    end
+
+    example "
+    123456789
+     [-------
+       [---)
+    " do
+      iv1 = Interval.new left_closed: 2, right_open: nil
+      iv2 = Interval.new left_closed: 4, right_open: 8
+      iv1.should overlap_with iv2
+      iv2.should overlap_with iv1
+    end
+
+    example "
+    123456789
+    ---------
+       [---)
+    " do
+      iv1 = Interval.new left_open: nil, right_open: nil
+      iv2 = Interval.new left_closed: 4, right_open: 8
+      iv1.should overlap_with iv2
+      iv2.should overlap_with iv1
+    end
+
+    example "
+    123456789
+    ---------
+    ---------
+    " do
+      iv1 = Interval.new left_open: nil, right_open: nil
+      iv2 = Interval.new left_open: nil, right_open: nil
+      iv1.should overlap_with iv2
+      iv2.should overlap_with iv1
+    end
+  end
+
+  describe 'properties' do
+    a = 0
+    b = 1
+
+    context 'open' do
+      subject { Interval.new left_open: a, right_open: b }
+      it { should_not be_empty }
+      it { should_not be_degenerate }
+      it { should     be_proper }
+      it { should     be_bounded }
+      it { should_not be_unbounded }
+
+      it { should     be_open }
+      it { should_not be_closed }
+
+      it { should     be_left_open }
+      it { should_not be_left_closed }
+      it { should     be_left_bounded }
+      it { should_not be_left_unbounded }
+
+      it { should     be_right_open }
+      it { should_not be_right_closed }
+      it { should     be_right_bounded }
+      it { should_not be_right_unbounded }
+
+      its(:left_endpoint)  { should be a }
+      its(:right_endpoint) { should be b }
+    end
+
+    context 'closed' do
+      subject { Interval.new left_closed: a, right_closed: b }
+      it { should_not be_empty }
+      it { should_not be_degenerate }
+      it { should     be_proper }
+      it { should     be_bounded }
+      it { should_not be_unbounded }
+
+      it { should_not be_open }
+      it { should     be_closed }
+
+      it { should_not be_left_open }
+      it { should     be_left_closed }
+      it { should     be_left_bounded }
+      it { should_not be_left_unbounded }
+
+      it { should_not be_right_open }
+      it { should     be_right_closed }
+      it { should     be_right_bounded }
+      it { should_not be_right_unbounded }
+
+      its(:left_endpoint)  { should be a }
+      its(:right_endpoint) { should be b }
+    end
+
+    context 'left-closed, right-open' do
+      subject { Interval.new left_closed: a, right_open: b }
+      it { should_not be_empty }
+      it { should_not be_degenerate }
+      it { should     be_proper }
+      it { should     be_bounded }
+      it { should_not be_unbounded }
+
+      it { should_not be_open }
+      it { should_not be_closed }
+
+      it { should_not be_left_open }
+      it { should     be_left_closed }
+      it { should     be_left_bounded }
+      it { should_not be_left_unbounded }
+
+      it { should     be_right_open }
+      it { should_not be_right_closed }
+      it { should     be_right_bounded }
+      it { should_not be_right_unbounded }
+
+      its(:left_endpoint)  { should be a }
+      its(:right_endpoint) { should be b }
+    end
+
+    context 'left-open, right-closed' do
+      subject { Interval.new left_open: a, right_closed: b }
+      it { should_not be_empty }
+      it { should_not be_degenerate }
+      it { should     be_proper }
+      it { should     be_bounded }
+      it { should_not be_unbounded }
+
+      it { should_not be_open }
+      it { should_not be_closed }
+
+      it { should     be_left_open }
+      it { should_not be_left_closed }
+      it { should     be_left_bounded }
+      it { should_not be_left_unbounded }
+
+      it { should_not be_right_open }
+      it { should     be_right_closed }
+      it { should     be_right_bounded }
+      it { should_not be_right_unbounded }
+
+      its(:left_endpoint)  { should be a }
+      its(:right_endpoint) { should be b }
+    end
+
+    context 'left-open, right-unbounded' do
+      [
+        {left_open: a, right_open:   nil},
+        {left_open: a, right_closed: nil},
+      ].each do |interval_def|
+        context do
+          subject { Interval.new interval_def }
+          it { should_not be_empty }
+          it { should_not be_degenerate }
+          it { should     be_proper }
+          it { should_not be_bounded }
+          it { should_not be_unbounded }
+
+          it { should     be_open }
+          it { should_not be_closed }
+
+          it { should     be_left_open }
+          it { should_not be_left_closed }
+          it { should     be_left_bounded }
+          it { should_not be_left_unbounded }
+
+          it { should     be_right_open }
+          it { should_not be_right_closed }
+          it { should_not be_right_bounded }
+          it { should     be_right_unbounded }
+
+          its(:left_endpoint)  { should be a }
+          its(:right_endpoint) { should be nil }
+        end
+      end
+    end
+
+    context 'left-unbounded, right-closed' do
+      [
+        {left_open:   nil, right_closed: b},
+        {left_closed: nil, right_closed: b},
+      ].each do |interval_def|
+        context do
+          subject { Interval.new interval_def }
+          it { should_not be_empty }
+          it { should_not be_degenerate }
+          it { should     be_proper }
+          it { should_not be_bounded }
+          it { should_not be_unbounded }
+
+          it { should_not be_open }
+          it { should_not be_closed }
+
+          it { should     be_left_open }
+          it { should_not be_left_closed }
+          it { should_not be_left_bounded }
+          it { should     be_left_unbounded }
+
+          it { should_not be_right_open }
+          it { should     be_right_closed }
+          it { should     be_right_bounded }
+          it { should_not be_right_unbounded }
+
+          its(:left_endpoint)  { should be nil }
+          its(:right_endpoint) { should be b }
+        end
+      end
+    end
+
+    context 'unbounded' do
+      subject { Interval.new left_open: nil, right_open: nil }
+      it { should_not be_empty }
+      it { should_not be_degenerate }
+      it { should     be_proper }
+      it { should_not be_bounded }
+      it { should     be_unbounded }
+
+      it { should     be_open }
+      it { should_not be_closed }
+
+      it { should     be_left_open }
+      it { should_not be_left_closed }
+      it { should_not be_left_bounded }
+      it { should     be_left_unbounded }
+
+      it { should     be_right_open }
+      it { should_not be_right_closed }
+      it { should_not be_right_bounded }
+      it { should     be_right_unbounded }
+
+      its(:left_endpoint)  { should be nil }
+      its(:right_endpoint) { should be nil }
+    end
+
+
+    context 'empty' do
+      [
+        {left_closed: b, right_closed: a},
+        {left_open:   a, right_open:   a},
+        {left_closed: a, right_open:   a},
+        {left_open:   b, right_closed: a},
+      ].each do |interval_def|
+        context do
+          subject { Interval.new(interval_def) }
+          it { should     be_empty }
+          it { should_not be_degenerate }
+          it { should_not be_proper }
+          it { should_not include(a) }
+          it { should_not include(b) }
+        end
+      end
+    end
+
+    context 'degenerate' do
+      subject { Interval.new(left_closed: a, right_closed: a) }
+      it { should     be_degenerate }
+      it { should_not be_empty }
+      it { should_not be_proper }
+      it { should include(a) }
+    end
+  end
+
+end
